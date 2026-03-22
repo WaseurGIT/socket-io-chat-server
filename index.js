31d83d16c3450390c54fac94648c30ed21bd32e4
WaseurGIT <waseurrahman31@gmail.com>
2026-03-22 20:42:30 +0600

    server is ready for a basic chat application

--- index.js
+++ index.js
@@ -13,17 +13,21 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
 
-  socket.on("send_message", (data) => {
-    console.log(data);
+  socket.on('join_room', (data)=>{
+    socket.join(data)
+    console.log(`user with ${socket.id} joined room ${data}`)
+  })
 
-    io.emit("receive_message", data);
-  });
+  socket.on('send_message', (data)=>{
+    socket.to(data.room).emit('receive_message', data)
+  })
 
   socket.on("disconnect", () => {
     console.log("user disconnected", socket.id);
